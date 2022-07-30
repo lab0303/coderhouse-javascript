6eee4b1b395786105f4b6b21b5e6addd215a601a
Luis Beltran <lab0303@hotmail.com>
2022-07-29 23:11:04 -0400

    agregando eventos

--- main.js
+++ main.js
@@ -43,11 +43,11 @@ agregarAlCarrito({ id: 1, nombre: "PES2022", plataforma: "PS5" });
 agregarAlCarrito({ id: 2, nombre: "CALL OF DUTY", plataforma: "PC" });
 agregarAlCarrito({ id: 3, nombre: "FIFA2022", plataforma: "XBOX" });
 agregarAlCarrito({ id: 4, nombre: "UFC4", plataforma: "PC" });
-console.log(carrito);
+//console.log(carrito);
 eliminarProducto("FIFA2022");
-console.log(carrito);
-agregarAlCarrito({ id: 4, nombre: "HALO", plataforma: "XBOX" });
-console.log(mostrarNombres(carrito));
+//console.log(carrito);
+agregarAlCarrito({ id: 5, nombre: "HALO", plataforma: "XBOX" });
+//console.log(mostrarNombres(carrito));
 
 /*let nombrePlataforma = prompt("Ingrese la plataforma ('PS5','XBOX','PC')");
 let consola = nombrePlataforma.toUpperCase();
@@ -63,12 +63,27 @@ const juegosConsola = filtrarProducto(carrito, consola);
 alert("Los juegos de " + consola + " son: " + mostrarNombres(juegosConsola));*/
 
 
-
+// Agregando Evento al DOM
 let juegos = document.querySelector(".juegosConsola");
+let cartAgregado = document.querySelector(".cartAgregado");
+
+function removerItem(){
+  console.log("Remover Item");
+}
 
 for(const juego of carrito){
-  console.log(juego);
+  let idCarrito = `idCart${juego.id}`;
   let juegoLista = document.createElement("div");
-  juegoLista.innerHTML = `<h2>${juego.nombre}--------${juego.plataforma}</h2>`;
+  juegoLista.innerHTML = `<h2>${juego.nombre}--------${juego.plataforma}</h2> <button id="${idCarrito}">Agregar al carrito</button>`;
   juegos.appendChild(juegoLista);
 }
+
+for(const juego of carrito){
+  let idCarrito = `idCart${juego.id}`;
+  document.getElementById(idCarrito).addEventListener("click",()=>{
+    let cartJuegos = document.createElement("div");
+    cartJuegos.innerHTML = `<h2>${juego.nombre}<button onclick="removerItem()">Sacar del carrito</button></h2>`;
+    cartAgregado.appendChild(cartJuegos);
+  });
+}
+
