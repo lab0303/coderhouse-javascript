debf1488620caa90a9ba086c02776c27da143d7f
Luis Beltran <lab0303@hotmail.com>
2022-08-07 19:06:55 -0400

    agregando operadore avanzados

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ const carrito = [];
 let cantidad = 0;
 //agregando productos al carrito
 function agregarAlCarrito(producto) {
+  const {nombre} = producto;
+  nombre && console.log(nombre); //Operador logico AND
   carrito.push(producto);
   cantidad++;
   console.log(
     "Agregaste " +
-      producto.nombre +
+      nombre +
       " al carrito, tienes " +
       cantidad +
       " producto(s)"
@@ -15,18 +17,18 @@ function agregarAlCarrito(producto) {
 //eliminando productos del carrito
 function eliminarProducto(producto) {
   let indice = carrito.findIndex((el) => el.nombre === producto);
-  if (indice != -1) {
-    carrito.splice(indice, 1);
-    cantidad--;
+  (indice != -1) ? (        // Operador Ternario
+    carrito.splice(indice, 1),
+    cantidad--,
     console.log(
       "Eliminaste " +
         producto +
         " de tu carrito, tienes " +
         cantidad +
         " producto(s)"
-    );
-  }
-}
+    )) : console.log("No existe ese producto");
+    }
+
 //mostrando los nombres de los productos del carrito
 function mostrarNombres(array) {
   const nombres = array.map((el) => el.nombre);
@@ -43,47 +45,62 @@ agregarAlCarrito({ id: 1, nombre: "PES2022", plataforma: "PS5" });
 agregarAlCarrito({ id: 2, nombre: "CALL OF DUTY", plataforma: "PC" });
 agregarAlCarrito({ id: 3, nombre: "FIFA2022", plataforma: "XBOX" });
 agregarAlCarrito({ id: 4, nombre: "UFC4", plataforma: "PC" });
-//console.log(carrito);
 eliminarProducto("FIFA2022");
-//console.log(carrito);
 agregarAlCarrito({ id: 5, nombre: "HALO", plataforma: "XBOX" });
-//console.log(mostrarNombres(carrito));
+eliminarProducto("GOW4");
 
-/*let nombrePlataforma = prompt("Ingrese la plataforma ('PS5','XBOX','PC')");
-let consola = nombrePlataforma.toUpperCase();
-let existePlataforma = carrito.some((el) => el.plataforma == consola);
-while (!existePlataforma) {
-  nombrePlataforma = prompt(
-    "Ingrese una plataforma exitente ('PS5','XBOX','PC')"
-  );
-  consola = nombrePlataforma.toUpperCase();
-  existePlataforma = carrito.some((el) => el.plataforma == consola);
-}
-const juegosConsola = filtrarProducto(carrito, consola);
-alert("Los juegos de " + consola + " son: " + mostrarNombres(juegosConsola));*/
 
 
 // Agregando Evento al DOM
 let juegos = document.querySelector(".juegosConsola");
 let cartAgregado = document.querySelector(".cartAgregado");
 
-function removerItem(){
-  console.log("Remover Item");
-}
+
 
 for(const juego of carrito){
+  const {nombre, plataforma} = juego; //Desestructuracion
   let idCarrito = `idCart${juego.id}`;
   let juegoLista = document.createElement("div");
-  juegoLista.innerHTML = `<h2>${juego.nombre}--------${juego.plataforma}</h2> <button id="${idCarrito}">Agregar al carrito</button>`;
+  juegoLista.innerHTML = `<h2>${nombre}--------${plataforma}</h2> <button id="${idCarrito}">Agregar al carrito</button>`;
   juegos.appendChild(juegoLista);
 }
-
+let idDelete = 0;
 for(const juego of carrito){
-  let idCarrito = `idCart${juego.id}`;
+  const {nombre, id} = juego;
+  let idCarrito = `idCart${id}`;
   document.getElementById(idCarrito).addEventListener("click",()=>{
+    idDelete++;
     let cartJuegos = document.createElement("div");
-    cartJuegos.innerHTML = `<h2>${juego.nombre}<button onclick="removerItem()">Sacar del carrito</button></h2>`;
+    cartJuegos.innerHTML = `<h2>${nombre}<button id="${idDelete}">Sacar del carrito</button></h2>`;
     cartAgregado.appendChild(cartJuegos);
+    document.getElementById(idDelete).addEventListener("click", () => {
+      cartAgregado.removeChild(cartJuegos);
+    })
   });
 }
+
+const filtrar = document.querySelector(".filtrar");
+filtrar.addEventListener("click", () => {
+  juegos.innerHTML = "";
+  const filtrado = carrito.filter((el)=> el.plataforma === "PC")
+  for(const juego of filtrado){
+    const {nombre, plataforma, id} = juego;
+    let idCarrito = `idCart${id}`;
+    let juegoLista = document.createElement("div");
+    juegoLista.innerHTML = `<h2>${nombre}--------${plataforma}</h2> <button id="${idCarrito}">Agregar al carrito</button>`;
+    juegos.appendChild(juegoLista);
+    document.getElementById(idCarrito).addEventListener("click",()=>{
+      idDelete++;
+      let cartJuegos = document.createElement("div");
+      cartJuegos.innerHTML = `<h2>${nombre}<button id="${idDelete}">Sacar del carrito</button></h2>`;
+      cartAgregado.appendChild(cartJuegos);
+      document.getElementById(idDelete).addEventListener("click", () => {
+        cartAgregado.removeChild(cartJuegos);
+      })
+    });
+  }
+})
+
+const cantidadCarrito = (carrito.length>0)? "Tienes elementos" : "No tienes elementos"; //Operador Ternario
+console.log(cantidadCarrito);
 
